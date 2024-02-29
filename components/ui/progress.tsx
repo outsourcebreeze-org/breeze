9d9db61e9f262af3dc314b0c45feef1c025ec772
Dimitry Desilias <info@outsourcebreeze.com>
2024-02-29 16:17:22 -0500

    Stripe Update 3

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -22,7 +22,7 @@ const Progress = React.forwardRef<
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
-))
+ ))
 Progress.displayName = ProgressPrimitive.Root.displayName
 
 export { Progress }
