1847c7ee1e5fccb2b4025c954b68934eee34e147
Dimitry Desilias <info@outsourcebreeze.com>
2024-03-01 17:25:49 -0500

    fix env variable

--- app/overview/models/train/page.tsx
+++ app/overview/models/train/page.tsx
@@ -24,7 +24,7 @@ export default async function Index() {
             Go Back
           </Button>
         </Link>
-        <VideoComponent apiKey={process.env.NEXT_PUBLIC_API_KEY || 'eyJzb3VsSWQiOiJkZG5hLW91dHNvdXJjZWJyZWV6ZWI2ZGQtLXRlc3RhcGkiLCJhdXRoU2VydmVyIjoiaHR0cHM6Ly9kaC5hei5zb3VsbWFjaGluZXMuY2xvdWQvYXBpL2p3dCIsImF1dGhUb2tlbiI6ImFwaWtleV92MV9iMGYzNGJiNS1kYmUyLTRhYjktYjBhMC0wMzIzZWI1MDg3OWIifQ=='} />
+        <VideoComponent apiKey={process.env.NEXT_PUBLIC_SOULMACHINE_API_KEY!} />
       </div>
     </div>
   );
