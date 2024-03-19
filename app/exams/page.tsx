913a4ae12614351ba9d317a290355b39db1bd22b
Dimitry Desilias <info@outsourcebreeze.com>
2024-03-19 11:45:07 -0400

    add HonorlockComponent

--- app/exams/page.tsx
+++ app/exams/page.tsx
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation";
 
 
 export const dynamic = "force-dynamic";
-import HonorlockWidget from "@/components/ui/HonorlockWidget";
+import HonorlockComponent from '../../components/HonorlockComponent';
 
 export default async function Index() {
    const supabase = createServerComponentClient({ cookies });
@@ -17,18 +17,10 @@ export default async function Index() {
   if (!user) {
     return redirect("/login");
   }
-  const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI1MyIsImp0aSI6IjY5ZmU4NGE5MmRlMDEzYzI2ZmY4ZTNjZTJlN2FiM2Y1NDM3NWQ3OTU4ZTRiMTVjMWYwMDBlOTU4NTYyNTNjMGExZDk5M2ZlYTQ5Y2QzMzk5IiwiaWF0IjoxNzEwNzkxNjcwLjg4MzE4MSwibmJmIjoxNzEwNzkxNjcwLjg4MzE4NSwiZXhwIjoxNzEwODc4MDcwLjg3MzA1Mywic3ViIjoiNzg0Iiwic2NvcGVzIjpbXX0.P1egvRHw9kQHaqBbm5EYqyI2NCHmP-ny-Hb25cAefrT_3gEYMUhqFqtofV9Csg4hSv0-RrmyMiT2hGJJrdOA9Vqz-4N7nbo5GRxj4qbFvoFwKXqYOJweoyL4b-bqj_Pd5TWTKKQjsgtKTo1QQ7OjFq82yYUrHYTYYMa62w81Z1CgjS5sZ1h6caDWa0woVZLzOVGEk-cPDAZP0bDloLsyTvCAPtgInRx5NHTz5a-0oWZt6zaboguqg3M2Rr-Kg-2PYdMOk2lrrMJW06C5UqZ2MetZjFJunQ_H46yTE0Ur5t3wgdQFucDFHsRFnTDqPWd1sh_Ytscj8sXQNpf5agI8v46IjKe5Q0if9eLBuZUp5mEm9rXAIwp9k14j3y8EJGWVitNm39cbFwUnrZGnT3dDxTVS66MT_gSWaQN10xbId-Yp321BO-citcGJCYfoza-YhPDmwHalhaZySXlFuT9eTjkOE96zGlbnwNTcCrGf7FBumPezDgw6VUMU5SETeKhGnxJDJEXahnonXwd4ih1_OYp3g2OnnqoDpdHT00OXksl-AnkkUpp4AC7V8t_8FCCmChf4NHNqePcq1Mvn3yaUzcKTe2AfKnpZgx_Tsoxb9H_bDC0GfOKJfyayrBsgPDZjcmMQNcCKAR45bU9033vf9lvfcZ9IZlshvYazyk9u95I'; // Remplacez par votre token réel
-  const contextType = 'course'; // Exemple de contextType
-  const contextId = 'c3831654-61cf-5f57-9a2b-8a9be58f99e1'; // Remplacez par votre contextId réel
-
-  return (
+    return (
     <div>
-      <h1>Examen avec Honorlock</h1>
-      <HonorlockWidget
-        token={token}
-        contextType={contextType}
-        contextId={contextId}
-      />
+      <h1>Page de Test Honorlock</h1>
+      <HonorlockComponent />
     </div>
   );
 };
---
+++ components/HonorlockComponent.tsx
@@ -0,0 +1,39 @@
+// components/HonorlockComponent.tsx
+import React, { useEffect } from 'react';
+
+// Assurez-vous que le type de fenêtre est étendu pour inclure HonorlockElements
+declare global {
+    interface Window {
+        HonorlockElements: HonorlockElements;
+    }
+}
+
+const HonorlockComponent: React.FC = () => {
+  useEffect(() => {
+    // Importe dynamiquement @honorlock/elements pour assurer la compatibilité avec SSR
+    import("@honorlock/elements").then(() => {
+      // Attendez que l'événement HonorlockElements soit disponible
+      document.addEventListener('HonorlockElements', () => {
+        // Initialisez Honorlock une fois que le composant est chargé
+        if (window.HonorlockElements) {
+          window.HonorlockElements.init({
+            token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI1MyIsImp0aSI6IjA0YWVmYzI3NzQ1MmFlYjdkOWQzNmQyM2Y2OTcxMmFjZmVlNzQwNzhjNzA4NzYwZWRiMDdlODcxY2M4Yjc5OGVjMjk4Yzg3YWUwNTAwMTYxIiwiaWF0IjoxNzEwODU1MTQ1LjUwNDM2NCwibmJmIjoxNzEwODU1MTQ1LjUwNDM2NywiZXhwIjoxNzEwOTQxNTQ1LjQ5Mjk5OCwic3ViIjoiNzg0Iiwic2NvcGVzIjpbXX0.Vxshv4UTCzkmNL6Xdv7ljkZ77zodRIFVzTf5inK-WCFHc8Vl4pBfV-6hF54PPunurX6XA081Y9vM4TdHf4bR478VbcQvTxv70mqyS92ADkdYLjQHyHEIs8ig-jabWJOhvdlLan9bn5jOC0KZ13i1GJlI92oOVnxk3adgQuY3y1gXyaxLRXTIW7XxAVAUyRloRmD9NE58ZcesUH0UJ5R8qAZ3Wg-RZ0mFxR4H8VIMu7QkiGssJsct7b6hMz9OxAvninitVuHMQ6g21efaqY8ka8j3AYVYs0fQxXAc9q67jDBbwuyuHrI0lhLOxnwORQmJSuOHmwKNQ7wV3fhSdZk4eKVHehC_jOgMadDBZdWkbcnlenbCRucvfLd2EmsmB4eCIMBgZKR1Ml6Xnb2C0jCRCwwl3ibMVnSEFPwOK31UFanYYPyLdDQ1gFMOasKmguQBFov9mt9kiDOpSGZvEVDXdVznGCpphtI0GzG9oPz5-nMEPNujAZcy5C23taPWymOM36ujpOiZam9lYY7FpLa4krn4m1SLM4VdhVTCQ3CpY_-8nyCdkG0-SOFqvMXWh71TyT0ZeYYuZQmJU0FE3vsdRAumh_hFvRUkWVga8Fn0Rr9DRXpvLhuSamgWFfkVkq7jvJvBV6HgFntyrf5mYxmasytP4WXK0gUdxWCrsUfjMBQ', // Remplacez par votre token réel
+            debug: true,
+            context: {
+              type: 'course',
+              id: '101', // Adaptez selon vos besoins
+            },
+          });
+        }
+      });
+    });
+  }, []);
+
+  return (
+    <div>
+      <honorlock-elements></honorlock-elements>
+    </div>
+  );
+};
+
+export default HonorlockComponent;
