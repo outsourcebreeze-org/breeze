c9e31fe11524544153e9882b7b23da57df2c3780
Dimitry Desilias <info@outsourcebreeze.com>
2024-03-20 14:42:23 -0400

    add HonorlockComponent9

--- components/HonorlockComponent.tsx
+++ components/HonorlockComponent.tsx
@@ -1,47 +1,45 @@
 // components/HonorlockComponent.tsx
 "use client";
+// pages/HonorlockComponent.tsx ou components/HonorlockComponent.tsx
 import React, { useEffect } from 'react';
+
+// declare global {
+//   interface Window {
+//     HonorlockElements: any; // Utilisez `any` ou définissez un type plus précis si disponible
+//   }
+// }
 
 const HonorlockComponent: React.FC = () => {
   useEffect(() => {
-    // Vérifie si le script de Honorlock a déjà été ajouté
-    const scriptId = 'honorlock-script';
-    if (document.getElementById(scriptId)) {
-      console.log("Script Honorlock déjà ajouté.");
-      return;
-    }
-
-    // Crée et ajoute le script de Honorlock à la page
-    const script = document.createElement('script');
-    script.id = scriptId;
-    script.type = 'module';
-    script.innerHTML = `
-      import '@honorlock/elements';
-      document.addEventListener('HonorlockElements', () => {
-          window.HonorlockElements.init({
+    // S'assure que le code s'exécute côté client
+    if (typeof window !== "undefined") {
+      // Charge dynamiquement @honorlock/elements
+      import('@honorlock/elements').then(() => {
+        // Initialisation de Honorlock
+        document.addEventListener('HonorlockElements', () => {
+          if (window.HonorlockElements) {
+            window.HonorlockElements.init({
               token: 'votre-token',
               debug: true,
               context: {
-                  type: 'course',
-                  id: '38a47339-d8ac-4fe6-8cf1-4404797af444',
+                type: 'course',
+                id: '38a47339-d8ac-4fe6-8cf1-4404797af444',
               },
-          });
-      });
-    `;
-    document.body.appendChild(script);
-
-    return () => {
-      // Nettoie le script lors du démontage du composant
-      script && document.body.removeChild(script);
-    };
+            });
+            console.log('Honorlock initialisé');
+          }
+        });
+      }).catch(err => console.error("Erreur lors du chargement de @honorlock/elements:", err));
+    }
   }, []);
 
   return (
     <div>
-      <p>This is a test page for Honorlock.</p>
+      <p>Ceci est une page de test pour Honorlock.</p>
       <honorlock-elements></honorlock-elements>
     </div>
   );
 };
 
 export default HonorlockComponent;
+
