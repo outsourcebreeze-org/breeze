7f73ccf09f65b072c3d7b75eb907985925af8db6
Dimitry Desilias <info@outsourcebreeze.com>
2024-03-20 12:28:01 -0400

    add HonorlockManager debug

--- components/HonorlockManager.tsx
+++ components/HonorlockManager.tsx
@@ -7,9 +7,9 @@ declare global {
 }
 
 const HonorlockManager: React.FC = () => {
-  const [token, setToken] = useState<string>('your_access_token');
+  const [token, setToken] = useState<string>('eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI2IiwianRpIjoiNDI2MTBhMmFiMDc4NGVjZDJmOWU2M2Q2ZWY5NGFiZjkxYjMxZTlhMmNiMDRjNDdhMTU5YjBlN2RhNGZkZTVkZDdmYjE2ZTIzZjQ4MDcwMzYiLCJpYXQiOjE3MTA5NTA1OTAuODA2MzIsIm5iZiI6MTcxMDk1MDU5MC44MDYzMjMsImV4cCI6MTcxMDk1MjM5MC43OTU3NDIsInN1YiI6IjcxMjM1Iiwic2NvcGVzIjpbXX0.owB0eo2pU3I5MwD27QNkfFAXiOvU-OhuMO6txT0-BgBtgZ1ltb7YRHOV8tei6-rc20h3379uDAPe5FPDP_jgOi_wDzpKHJcQfYMo0RqsV5xh2oWsFpcovaAuL87OdUfmP4XemQUw3v4QfRoB-kUthwrWpbIoX7BpqUfYk210FNyUuW5wfjqeCzPfwpsaBku7zPwn8tiP1FVRpxIcaAJf6EvPtCsmMiUB1gAVhpLL_H_o5cVptXSeDIghQvjuMIJZOGRV18Co3NeUKAAiI-2zRSyRck7dYqMBk6NswXCuneQ6e8CjMI60CYiI80WjJdI8pCCeNLvBZ1ydQ-Cmww4rvLiCmykUNvJfjvXGMHyduKJ2yn9kqT7TK3kuq90Jzno3c7ZA9vjBHZyL2TBbsvzFcz3WPXGCDnC732qh2TfiVpX0pWCa26kG4TCy17RAzws7zc3wDvX1iCQGUJjFddIkKp9iG2ko_7IFlbYhsY20r_7F9mTOMUwMaDk2v9V90Y1DHpp4-O_0qzKA-JS3rj4NRWIx7CnGg5n44TOEruxHOE4VemO36a924iANdys9KGBmNzfsG8dcikX2UmdU54RrrOqgUMMoYUy1CxKaHi3QZGNQTKL7AXhiZHIKqid02J3N7k7Q9w8VwGB0Jx6gU-kmBEIrIs3Urox2bSVDdBb3ExA');
   const [contextType, setContextType] = useState<string>('course');
-  const [contextId, setContextId] = useState<string>('default_course_uuid');
+  const [contextId, setContextId] = useState<string>('38a47339-d8ac-4fe6-8cf1-4404797af444');
   const [width, setWidth] = useState<number>(500);
   const [height, setHeight] = useState<number>(500);
   const contextOptions = ['course', 'session', 'exam_results', 'exam_settings', 'search_and_destroy'];
@@ -36,6 +36,7 @@ const HonorlockManager: React.FC = () => {
           width,
           height,
         });
+        console.log("HonorlockElements initialized");
       });
     }).catch(err => console.error("Failed to load @honorlock/elements", err));
   }, [token, contextType, contextId, width, height]);
