5f2eac4a90e66ca678932e06a8bd84b045ea2f4f
Gab Labelle <gab@visualise.ca>
2024-03-21 10:16:58 -0400

    Honorlock in now loading in React component

--- app/exams/page.tsx
+++ app/exams/page.tsx
@@ -1,26 +1,30 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import dynamic from "next/dynamic";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-
-
-export const dynamic = "force-dynamic";
-import HonorlockComponent from '../../components/HonorlockComponent';
+const HonorlockComponent = dynamic(
+  () => import("../../components/HonorlockComponent"),
+  {
+    ssr: false,
+  }
+);
 
 export default async function Index() {
-   const  supabase = createServerComponentClient({ cookies });
-  
-    const {
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
     data: { user },
   } = await supabase.auth.getUser();
 
   if (!user) {
     return redirect("/login");
   }
-    return (
+
+  return (
     <div>
       <h1>Page de Test Honorlock</h1>
       <HonorlockComponent />
     </div>
   );
-};
+}
--- components/HonorlockComponent.tsx
+++ components/HonorlockComponent.tsx
@@ -1,25 +1,21 @@
 "use client";
 
-import React, { useEffect } from 'react';
-import '@honorlock/elements';
-
+import React, { useEffect } from "react";
+import "@honorlock/elements";
 
 const HonorlockComponent: React.FC = () => {
   useEffect(() => {
-    console.log('test');
-        document.addEventListener('HonorlockElements', () => {
-          console.log('Honorlock initialisé');
-            window.HonorlockElements.init({
-              token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI2IiwianRpIjoiYjAyNmU3ODFjNGMzNjMwODc4YTBhMjkxMmQyNDIxMTA3MTM4OWU5MDUzNGMyZGE5MDZlZjFhNjI4N2E0NjEwMjQzNDFkY2Y2MWZmMjUwNTMiLCJpYXQiOjE3MTA5NjA0MjYuOTg3NjM4LCJuYmYiOjE3MTA5NjA0MjYuOTg3NjQyLCJleHAiOjE3MTA5NjIyMjYuOTcwMzIyLCJzdWIiOiI3MTIzNSIsInNjb3BlcyI6W119.EntwH1Rkmk_wP838bDTZGfraLrPc8DlAEDEJ1BmlrhT7VS1TWSCymDhptCpXoYW15QNX0xvVtYJLwkQgp5kcbyxCAfta7RZ-1FBcAZf0HTVF3CwIFLodSnDPDd2iosnvG7a8ykD4Sh2Xq25A1QGlJ7jz9dl6TLjcAyUzHbRq_TbXp9i1MddR6kajBPBQKqwkee_T7DebOvvWKSoxyYw8McAzYVV9PtrJ1qB8JTftU2GLT818zh61TesON_XU542vHXMtrxrZTeVUCv62k0Nwck7XlnZsuNkY2RIeU2idZtMxobTBDtZf82gAU7jyC-pft8ztUa7m02SP9gm5htLigEM1wFfO7O2u9KMr6H4KJgPwAZZMiH4WZirmbUIIPIeFBf2ZdUKW73lOqxLR8Aw1liOK1ymSMeak8umttd3A89o35Oa1u5w2P-Lw-pRiWt7Zc8wJx6spCkJB12I5HMu5atulRkLDaVgxkEUwKn9IaMCuEt6W0dQT9B7RvA6lPeTZqHfj0Ng5QLPC1_s3gmgsgO8w9NDIiofkOi1rjGULBzh3Moqan9k-RLVEQBzRRdU03zA20YvxH-8k_xKktk9ghEo6taEfAuQB7fptMkHY8eVSjyEapfhyFTqADrze-uwBzy9kcxV6un_nhEzIaZTSXtCsu9dIpF8xJeq6sLKz7AA',
-              debug: true,
-              context: {
-                type: 'course',
-                id: '38a47339-d8ac-4fe6-8cf1-4404797af444',
-              },
-            });     
-        }
-          
-        );
+    setTimeout(() => {
+      window.HonorlockElements.init({
+        token:
+          "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI2IiwianRpIjoiYjAyNmU3ODFjNGMzNjMwODc4YTBhMjkxMmQyNDIxMTA3MTM4OWU5MDUzNGMyZGE5MDZlZjFhNjI4N2E0NjEwMjQzNDFkY2Y2MWZmMjUwNTMiLCJpYXQiOjE3MTA5NjA0MjYuOTg3NjM4LCJuYmYiOjE3MTA5NjA0MjYuOTg3NjQyLCJleHAiOjE3MTA5NjIyMjYuOTcwMzIyLCJzdWIiOiI3MTIzNSIsInNjb3BlcyI6W119.EntwH1Rkmk_wP838bDTZGfraLrPc8DlAEDEJ1BmlrhT7VS1TWSCymDhptCpXoYW15QNX0xvVtYJLwkQgp5kcbyxCAfta7RZ-1FBcAZf0HTVF3CwIFLodSnDPDd2iosnvG7a8ykD4Sh2Xq25A1QGlJ7jz9dl6TLjcAyUzHbRq_TbXp9i1MddR6kajBPBQKqwkee_T7DebOvvWKSoxyYw8McAzYVV9PtrJ1qB8JTftU2GLT818zh61TesON_XU542vHXMtrxrZTeVUCv62k0Nwck7XlnZsuNkY2RIeU2idZtMxobTBDtZf82gAU7jyC-pft8ztUa7m02SP9gm5htLigEM1wFfO7O2u9KMr6H4KJgPwAZZMiH4WZirmbUIIPIeFBf2ZdUKW73lOqxLR8Aw1liOK1ymSMeak8umttd3A89o35Oa1u5w2P-Lw-pRiWt7Zc8wJx6spCkJB12I5HMu5atulRkLDaVgxkEUwKn9IaMCuEt6W0dQT9B7RvA6lPeTZqHfj0Ng5QLPC1_s3gmgsgO8w9NDIiofkOi1rjGULBzh3Moqan9k-RLVEQBzRRdU03zA20YvxH-8k_xKktk9ghEo6taEfAuQB7fptMkHY8eVSjyEapfhyFTqADrze-uwBzy9kcxV6un_nhEzIaZTSXtCsu9dIpF8xJeq6sLKz7AA",
+        debug: true,
+        context: {
+          type: "course",
+          id: "38a47339-d8ac-4fe6-8cf1-4404797af444",
+        },
+      });
+    }, 0);
   }, []);
 
   return (
@@ -30,4 +26,3 @@ const HonorlockComponent: React.FC = () => {
 };
 
 export default HonorlockComponent;
-
